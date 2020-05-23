87271ea51ffe2ad0d416a716da9fcbb7a1f801a4
joann129 <a0905187634@gmail.com>
2020-05-23 22:48:20 +0800

    optab 可以讀取 赫序表(全部ascii碼相加取個位數)印出

--- main.c
+++ main.c
@@ -1,26 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define max 10
+#define opcodeNameMax 7
 
-struct optab_Node{
-    char name[6];
+struct optab_Node
+{
+    char name[opcodeNameMax];
     int format;
     int opcode;
     struct optab_Node * next;
 };
 typedef struct optab_Node optab_node;
-optab_node optabHeader[10];
+optab_node * optabHeader[10];
 
-void create_Optab(optab_node* node) {
-    FILE * fp_Optab = fopen("optab.txt");
-    while(!feof(fp))
+optab_node * newNode(void)
+{
+    optab_node * add = malloc(sizeof(optab_node));
+    add->next = NULL;
+    return add;
+}
+
+void print(optab_node * head) {
+    optab_node * ptr;
+    if(head->next == NULL) {
+        printf("empty\n");
+    }else{
+        ptr = head->next;
+        while(ptr != NULL) {
+            printf("%s\n", ptr->name);
+            ptr = ptr->next;
+        }
+    }
+    printf("\n");
+}
+
+void create_Optab(void)
+{
+    optab_node* ptr;
+    int i, sum = 0, readCol;
+    for(i = 0; i < max; i ++)
+    {
+        optabHeader[i] = newNode();
+    }
+    FILE * fp_Optab = fopen("optab.txt", "r");
+    char str_op[opcodeNameMax];
+    while(1) {
+        fscanf(fp_Optab, "%s", str_op);
+        if(feof(fp_Optab) != 0) break;
+        for(i = 0; i < strlen(str_op); i++) {
+            sum += str_op[i];
+        }
+        sum %= 10;
+        ptr = optabHeader[sum];
+        while(ptr->next != NULL) {
+            ptr = ptr->next;
+        }
+        ptr->next = newNode();
+        ptr = ptr->next;
+        strcpy(ptr->name, str_op);
+        fscanf(fp_Optab, "%s", str_op);
+        //ptr->format = atoi(str_op);
+        fscanf(fp_Optab, "%s", str_op);
+        ptr->opcode = atoi(str_op);
+        sum = 0;
+    }
+    fclose(fp_Optab);
+    for(i = 0; i < max; i++) {
+        print(optabHeader[i]);
+    }
+    //print
 }
 
 int main()
 {
-    FILE * fp;
-    fp = fopen("srcpro.txt", "r");
+    create_Optab();
+    FILE * fp = fopen("srcpro.txt", "r");
     char str[20];
-    while(1) {
+    while(1)
+    {
         fscanf(fp, "%s", str);
         if(feof(fp) != 0) break;
         printf("%s\n", str);
