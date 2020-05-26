7259d3286147c8a4af3ec32fff30c64102ac10c1
joann129 <a0905187634@gmail.com>
2020-05-26 15:35:01 +0800

    symtab find and insert not finished

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <math.h>
-#define opcodeHeaderMax 10
+#define headerMax 10
 #define opcodeNameMax 7
 #define opcodeFormatyMax 4
+#define symtabNameMax 7
 #define srcMax 33
 #define srcTagMax 6
 #define srcCodeMax 6
@@ -18,16 +20,60 @@ struct optab_Node
     struct optab_Node * next;
 };
 typedef struct optab_Node optab_node;
-optab_node * optabHeader[opcodeHeaderMax];
+optab_node * optabHeader[headerMax];
 
-optab_node * newNode(void)
+struct symtab_Node{
+    char name[symtabNameMax];
+    int loc;
+    struct symtab_Node * next;
+};
+typedef struct symtab_Node symtab_node;
+symtab_node * symtabHeader[headerMax];
+
+
+char *trim(char *str)
+{
+	if (str == NULL || *str == '\0')
+	{
+		return str;
+	}
+	int len = strlen(str);
+	char *p = str + len - 1;
+	while (p >= str  && isspace(*p))
+	{
+		*p = '\0';
+		--p;
+	}
+
+	return str;
+}
+
+int key(char name[opcodeNameMax])
+{
+    int i, sum = 0;
+    for(i = 0; i < strlen(name); i++)
+    {
+        sum += name[i];
+    }
+    sum %= 10;
+    return sum;
+
+}
+
+optab_node * optabNewNode(void)
 {
     optab_node * add = malloc(sizeof(optab_node));
     add->next = NULL;
     return add;
 }
 
-void print(optab_node * head)
+symtab_node * symtabNewNode(void) {
+    symtab_node * add = malloc(sizeof(symtab_node));
+    add->next = NULL;
+    return add;
+}
+
+void optabPrint(optab_node * head)
 {
     optab_node * ptr;
     if(head->next == NULL)
@@ -46,13 +92,13 @@ void print(optab_node * head)
     printf("\n");
 }
 
-void create_Optab(void)
+void optabCreate(void)
 {
     optab_node* ptr;
-    int i, sum = 0;
-    for(i = 0; i < opcodeHeaderMax; i ++)
+    int i;
+    for(i = 0; i < headerMax; i ++)
     {
-        optabHeader[i] = newNode();
+        optabHeader[i] = optabNewNode();
     }
     FILE * fp_Optab = fopen("optab.txt", "r");
     int opcode;
@@ -61,27 +107,75 @@ void create_Optab(void)
     {
         fscanf(fp_Optab, "%s %s %x", name, format, &opcode);
         if(feof(fp_Optab) != 0) break;
-        for(i = 0; i < strlen(name); i++)
-        {
-            sum += name[i];
-        }
-        sum %= 10;
-        ptr = optabHeader[sum];
+//        for(i = 0; i < strlen(name); i++)
+//        {
+//            sum += name[i];
+//        }
+//        sum %= 10;
+        ptr = optabHeader[key(name)];
         while(ptr->next != NULL)
         {
             ptr = ptr->next;
         }
-        ptr->next = newNode();
+        ptr->next = optabNewNode();
         ptr = ptr->next;
         strcpy(ptr->name, name);
         strcpy(ptr->format, format);
         ptr->opcode = opcode;
-        sum = 0;
     }
     fclose(fp_Optab);
-    for(i = 0; i < opcodeHeaderMax; i++)
+    for(i = 0; i < headerMax; i++)
     {
-        print(optabHeader[i]);
+        optabPrint(optabHeader[i]);
+    }
+}
+
+void symtabPrint(symtab_node * head)
+{
+    symtab_node * ptr;
+    if(head->next == NULL)
+    {
+        printf("empty\n");
+    }
+    else
+    {
+        ptr = head->next;
+        while(ptr != NULL)
+        {
+            printf("%6s %04d\n", ptr->name, ptr->loc);
+            ptr = ptr->next;
+        }
+    }
+    printf("\n");
+}
+
+void symtabInsert(int index, char name[symtabNameMax], int loc) {
+    symtab_node * ptr;
+    ptr = symtabHeader[index];
+    while(ptr->next != NULL) {
+        ptr = ptr->next;
+    }
+    ptr->next = symtabNewNode();
+    ptr = ptr->next;
+    strcpy(ptr->name, name);
+    ptr->loc = loc;
+}
+
+int symtabFind(int index, char tag[srcTagMax]) {
+    symtab_node * ptr = symtabHeader[index];
+    while(ptr->next != NULL) {
+        if(strncmp(ptr->name, tag, 6) == 0) {
+            return 1;
+        }
+        ptr = ptr->next;
+    }
+    return 0;
+}
+
+void symtabCreate(void) {
+    int i;
+    for(i = 0; i < headerMax; i++) {
+        symtabHeader[i] = symtabNewNode();
     }
 }
 
@@ -95,12 +189,13 @@ void create_Optab(void)
 
 int main()
 {
-    create_Optab();
+    optabCreate();
+    symtabCreate();
     FILE * fp_input = fopen("srcpro.txt", "r");
     FILE * fp_output = fopen("intermediate.txt", "w");
     char srcStr[srcMax], srcTag[srcTagMax], srcCode[srcCodeMax+1], srcOperand[srcOperandMax+1];
     int locctr, startLoc;
-    int flag = 0, i, sum = 0, srcOper;
+    int flag = 0, srcOper, keyTemp;
     optab_node * ptr;
     while(fgets(srcStr, srcMax, fp_input) != NULL)
     {
@@ -124,33 +219,56 @@ int main()
             fputs("\n", fp_output);
             continue;
 //            printf("%d\n", locctr);
-        }else{
+        }
+        else
+        {
             locctr = 0;
         }
-        if(strncmp(srcCode, "END", 3) != 0) {
+        if(strncmp(srcCode, "END", 3) != 0)
+        {
 
-                for(i = 0; i < strlen(srcCode); i++) {
-                    sum += srcCode[i];
-                }
-                sum %= 10;
-                ptr = optabHeader[sum];
-                if(strncmp(srcCode, "WORD", 4) == 0) {
+//            for(i = 0; i < strlen(srcCode); i++)
+//            {
+//                sum += srcCode[i];
+//            }
+//            sum %= 10;
+
+//            keyTemp = key(srcTag);
+//            if(!symtabFind(keyTemp, srcTag)) {
+//                symtabInsert(keyTemp, srcTag, locctr);
+//            }else{
+//                printf("輸入檔有重複符號\n");
+//            }
+//            symtabPrint(symtabHeader[keyTemp]);
+            ptr = optabHeader[key(srcCode)];
+            if(strncmp(srcCode, "WORD", 4) == 0)
+            {
+                locctr += 3;
+            }
+            else if(strncmp(srcCode, "RESW", 4) == 0)
+            {
+                locctr += 3 * srcOper;
+            }
+            else if(strncmp(srcCode, "RESB", 4) == 0)
+            {
+                locctr += srcOper;
+            }
+            else if(strncmp(srcCode, "BYTE", 4) == 0)
+            {
+
+            }
+            while(ptr->next != NULL)
+            {
+                if(strncmp(ptr->name, srcCode, strlen(ptr->name)) == 0)
+                {
                     locctr += 3;
-                }else if(strncmp(srcCode, "RESW", 4) == 0) {
-                    locctr += 3 * srcOper;
-                }else if(strncmp(srcCode, "RESB", 4) == 0) {
-                    locctr += srcOper;
-                }else if(strncmp(srcCode, "BYTE", 4) == 0) {
-
+                    break;
                 }
-                while(ptr->next != NULL) {
-                    if(strncmp(ptr->name, srcCode, strlen(ptr->name)) == 0) {
-                        locctr += 3;
-                        break;
-                    }else{
-                        ptr = ptr->next;
-                    }
+                else
+                {
+                    ptr = ptr->next;
                 }
+            }
 
         }
     }
